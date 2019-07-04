b7449e0ec9d2dae5d724ecb3574baaed513945c2
Heva <he.wu@linecorp.com>
2019-07-04 18:45:09 +0900

    Add test code

--- CSVReader.py
+++ CSVReader.py
@@ -1,105 +1,61 @@
 import csv
+from enum import Enum
+
+from Error import Error
+
+class CSVFileName(Enum):
+    sheet1 = "HealthyFood.csv"
+    sheet2 = "Weather.csv"
+
+class CSVColumnName(Enum):
+    name = "Name"
+    types = "Types"
+    category = "Category"
+    location = "Location"
+    weather = "Weather Forecasts"
 
 class CSVReader():
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.dataSource = []
+        self.fieldNames = []
+        self._readCSVFile()
+
     @staticmethod
     def _formatArray(array):
         """Remove duplicate elements"""
         array = list(set(array))
         """Remove empty elements"""
         array = list(filter(None, array))
-        """Remove \n from all elements"""
-        array = list(map(str.strip, array))
         return array
-
-    @staticmethod
-    def _parseScreens(dataSource):
-        screens = []
-        for item in dataSource:
-            screens.append(item[ScreenCSVReader.screenColumnName])
-        screens = CSVReader._formatArray(screens)
-        return screens
-
-class ScreenCSVReader():
-    screenColumnName = "screen"
-    tsEventColumnName = "TS_event_name"
-
-    def __init__(self):
-        self.dataSource = []
-        self.screens = []
-        self.tsEventNameList = None
-        self._readCSVFile()
-
-    def _readCSVFile(self):
-        with open('Screen.csv', mode='r', newline='') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-
-            """Parse Data"""
-            for row in csv_reader:
-                self.dataSource.append(row)
-
-        """Retrive Screen List"""
-        self.screens = CSVReader._parseScreens(self.dataSource)
-
-        """Retrieve TS Event Name"""
-        self.tsEventName = self.dataSource[1]["TS_event_name"]
-
-        """Format Screen Name List (remove duplicate & empty value)"""
-        self.screens = CSVReader._formatArray(self.screens)
-
-    def getScreens(self):
-        print(self.screens)
-        return self.screens
-
-    def getTSEventName(self):
-        print(self.tsEventName)
-        return self.tsEventName
-
-"""Read ClickEvent.csv or """
-class ClickEventCSVReader():
-    overViewColumnName = "overview"
-    screenColumnName = "screen"
-    actionColumnName = "action"
-    labelColumnName = "label"
-    tsEventColumnName = "TS_event_name"
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.dataSource = []
-        self.tsEventName = None
-        self._readCSVFile()
 
     def _readCSVFile(self):
         with open(self.filename, mode='r', newline='') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-
-            """Parse Data(overview, screen, action, label)"""
+            """Parse Column Name"""
+            self.fieldNames = csv_reader.fieldnames
             for row in csv_reader:
                 data = {}
-                data[ClickEventCSVReader.overViewColumnName] = row[ClickEventCSVReader.overViewColumnName]
-                data[ClickEventCSVReader.screenColumnName] = row[ClickEventCSVReader.screenColumnName]
-                data[ClickEventCSVReader.actionColumnName] = row[ClickEventCSVReader.actionColumnName]
-                data[ClickEventCSVReader.labelColumnName] = row[ClickEventCSVReader.labelColumnName]
-                data[ClickEventCSVReader.tsEventColumnName] = row[ClickEventCSVReader.tsEventColumnName]
-                self.dataSource.append(data)
+                for csvName in CSVColumnName:
+                    if csvName.value in self.fieldNames:
+                        """Use .strip to remove \n"""
+                        data[csvName.value] = str(row[csvName.value]).strip()
+                if data:
+                    self.dataSource.append(data)
 
-        """Retrieve TS Event Name"""
-        self.tsEventName = self.dataSource[1]["TS_event_name"]
+    def getColumn(self, columnName):
+        if columnName in self.fieldNames:
+            column = [item[columnName] for item in self.dataSource]
+            column = CSVReader._formatArray(column)
+            print(column)
+            return column
+        else:
+            raise Error("[CSVReader] '%s' is not exist in '%s' file" % (columnName, self.filename))
 
-    def getScreens(self):
-        screens = CSVReader._parseScreens(self.dataSource)
-        print(screens)
-        return screens
-
-    def getClickEventDict(self):
-        print(self.dataSource)
-        return self.dataSource
-
-    def getTSEventName(self):
-        print(self.tsEventName)
-        return self.tsEventName
-
-rd = ClickEventCSVReader('ClickEvent.csv')
-rd.getScreens()
-rd.getClickEventDict()
-rd.getTSEventName()
-
+    def getDataSource(self):
+        if self.dataSource:
+            print(self.dataSource)
+            return self.dataSource
+        else:
+            raise Error("[CSVReader] '%s' doesn't contain valid params. Please checking it again." % (self.filename))
--- generateCSV.py
+++ generateCSV.py
@@ -1,13 +1,34 @@
 import pandas as pd
-import xlrd
 
-def dump_csv(filename):
-    workbook = xlrd.open_workbook(filename)
-    sheetNames = workbook.sheet_names()
-    for sheetName in sheetNames:
-        print(sheetName)
-        outfilename = "%s.csv" % (sheetName)
-        sheet_data = pd.read_excel(filename, sheetName, index_col = None)
-        sheet_data.to_csv(outfilename, encoding = 'utf-8', index = False)
+from Error import Error
+from CSVReader import CSVReader, CSVColumnName, CSVFileName
 
-dump_csv('test_Tracking.xlsx')
+class Autogenerator():
+    def __init__(self, filename):
+        self.filename = filename
+        self.generateSwiftFile()
+
+    def _getCSVDataSource(self, csvFileName):
+        rd = CSVReader(csvFileName)
+        return rd.getDataSource()
+
+    def dump_csv(self):
+        workbook = pd.ExcelFile(self.filename)
+        sheetNames = workbook.sheet_names
+        for sheetName in sheetNames:
+            print(sheetName)
+            outfilename = "%s.csv" % (sheetName)
+            sheet_data = pd.read_excel(self.filename, sheetName, index_col = None)
+            sheet_data.to_csv(outfilename, encoding = 'utf-8', index = False)
+
+    def generateSwiftFile(self):
+        try:
+            self.dump_csv()
+            print("HealtyFood Sheet: ")
+            print(self._getCSVDataSource(CSVFileName.sheet1.value))
+            print("Weather Sheet: ")
+            print(self._getCSVDataSource(CSVFileName.sheet2.value))
+        except Error as e:
+            print("Received Error: ", e.data)
+
+Autogenerator('TestExcel.xlsx')
